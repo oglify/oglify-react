73e9e96f95735cea69718c334e033037173dca5c
brendansafr <brendansafr@gmail.com>
2022-01-16 16:26:32 -0500

    Draft 1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "oglify:react",
-  version: "0.0.1",
+  version: "0.0.2",
   summary: "The react display components for OGLify Meteor",
   git: "https://github.com/oglify/oglify-react",
   documentation: "README.md",
@@ -14,12 +14,12 @@ Npm.depends({
 
 Package.onUse(function (api) {
   api.versionsFrom("2.5.3");
-  api.use(["ecmascript", "oglify:react-theme@0.0.1"]);
+  api.use(["ecmascript", "oglify:react-theme@0.0.2"]);
   api.mainModule("oglify-react.js");
 });
 
 Package.onTest(function (api) {
-  api.use(["ecmascript", "oglify:react-theme@0.0.1"]);
+  api.use(["ecmascript", "oglify:react-theme@0.0.2"]);
   api.use("tinytest");
   api.use("oglify:react");
   api.mainModule("oglify-react-tests.js");
